9bf7ce3526af7b33174d8b76237bfefbcb58a4c3
Ida Bosch <imbosch@stud.ntnu.no>
2018-04-10 19:26:10 +0200

    Added test suite

---
+++ Tests/AllTestSuite.java
@@ -0,0 +1,9 @@
+import UnitTests.*;
+import org.junit.runners.Suite;
+import org.junit.runner.RunWith;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({PersonTest.class, VehicleTest.class})
+
+public class AllTestSuite {
+}
