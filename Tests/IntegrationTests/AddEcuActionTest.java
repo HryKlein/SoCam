d0afd5e3d8286698091984fb0787be9dff4eb565
Ida Bosch <imbosch@stud.ntnu.no>
2018-04-20 13:46:46 +0200

    addecuaction test

--- Tests/IntegrationTests/AddEcuActionTest.java
+++ Tests/IntegrationTests/AddEcuActionTest.java
@@ -4,20 +4,30 @@ import no.ntnu.fp.gui.AddEcuAction;
 import no.ntnu.fp.gui.FactoryProjectPanel;
 import no.ntnu.fp.gui.NWPEcuPanel;
 import no.ntnu.fp.gui.NewVehiclePanel;
-import no.ntnu.fp.model.FactoryProject;
+import no.ntnu.fp.gui.EcuListModel;
+import no.ntnu.fp.model.Vehicle;
 import org.junit.Test;
-
+import javax.swing.*;
+import static org.junit.Assert.*;
 import java.awt.event.ActionEvent;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class AddEcuActionTest {
 
     @Test
-    public void actionPerformedTest(){
+    public void actionPerformedTest() throws MalformedURLException {
         NWPEcuPanel panel = new NWPEcuPanel(new NewVehiclePanel(new FactoryProjectPanel()));
         AddEcuAction action = new AddEcuAction(panel);
+        panel.setModel(new EcuListModel(new Vehicle(), new URL("http://www.google.no")));
 
-        action.actionPerformed(new ActionEvent(null, 0, ""));
+        int oldEcuId = panel.getModel().getProject().getLargestEcuId();
 
-        panel.
+        action.actionPerformed(new ActionEvent(new JButton(), 0, ""));
+
+        int newEcuId = panel.getModel().getProject().getLargestEcuId();
+
+        assertEquals(oldEcuId + 1, newEcuId);
+
     }
 }
